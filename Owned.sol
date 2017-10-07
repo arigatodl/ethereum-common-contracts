a76d374457c07f0e41628fccff950568414fc282
Dulguun Batmunkh <dulguun.mongolia@yahoo.com>
2017-10-08 05:53:56 +0900

    Owned initial
    - initial push of Owned contract

---
+++ Owned.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.13;
+
+contract Owned {
+    address public owner;
+
+    event LogOwnerSet(address indexed previousOwner, address indexed newOwner);
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    function setOwner(address newOwner)
+        public
+        fromOwner
+        returns(bool success)
+    {
+        require(owner != newOwner);
+        require(newOwner != address(0));
+
+        LogOwnerSet(owner, newOwner);
+        owner = newOwner
+
+        return true;
+    }
+
+    modifier fromOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
